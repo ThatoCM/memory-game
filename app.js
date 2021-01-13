99b558c6564842d35d575575a05da85332e52ac4
Thato <crazytha2@hotmail.co.uk>
2021-01-13 00:03:58 +0000

    initial commit of a memory game from a javascrip youtube tutorial on myfreecodecamp channel

---
+++ app.js
@@ -0,0 +1,70 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    //card options
+    const cardArray = [
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+        {
+            name: 'fries',
+            img: 'images/fries.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png'
+        },
+        {
+            name: 'ice-cream',
+            img: 'images/ice-cream.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
+        },
+        {
+            name: 'hotdog',
+            img: 'images/hotdog.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png'
+        },
+        {
+            name: 'pizza',
+            img: 'images/pizza.png'
+        },
+        {
+            name: 'burger',
+            img: 'images/burger.png'
+        },
+        {
+            name: 'burger',
+            img: 'images/burger.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'images/milkshake.png'
+        },
+        {
+            name: 'milkshake',
+            img: 'images/milkshake.png'
+        }
+        
+    ]
+
+    const grid = document.querySelector('.grid')
+    
+    //create the board
+    function createBoard() {
+        for (let i = 0; i < cardArray.length; i++) {
+            var card = document.createElement('img')
+            card.setAttribute('src', 'images/blank.png')
+            card.setAttribute('data-id', i)
+            //card.addEventListener('click', flipcard)
+            grid.appendChild(card)
+        }
+    }
+
+    createBoard()
+})
